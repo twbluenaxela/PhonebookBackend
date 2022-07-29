0a720f449bc6c8bbbc6a8719d5fff488ea975733
Nick Yu <rednaxela64@outlook.com>
2022-07-29 15:43:11 -0700

    Add build scripts

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 app.use(express.json());
 app.use(cors())
-// app.use(express.static("build"));
+app.use(express.static("build"));
 // app.use(requestLogger)
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
