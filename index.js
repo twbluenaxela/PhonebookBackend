671bce04687b2315fc990220f0935aae90962700
Nick Yu <rednaxela64@outlook.com>
2022-07-28 22:40:42 -0700

    Reinstall dependencies

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 
 app.use(express.json())
+app.use(express.static('build'))
 // app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
