fce5b530a4ca49a0bdf5d12933a3d0d3d87e390e
阿洋 <rednaxela64@outlook.com>
2022-07-31 01:09:25 +0000

    Add get and add functions to mongodb

--- mongo.js
+++ mongo.js
@@ -5,7 +5,16 @@ const name = process.argv[3]
 const number = process.argv[4]
 const databaseName = "phonebookApp";
 
-const url = `mongodb+srv://admin:${password}@cluster0.yjj12od.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+if (process.argv.length < 3) {
+  console.log("Current argv length", process.argv.length);
+  console.log(
+    `Please provide the password as an argument: node mongo.js <password>`
+  );
+  process.exit(1);
+}
+
+// const url = `mongodb+srv://admin:${password}@cluster0.yjj12od.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+const url = `mongodb+srv://admin:${password}@cluster0.yjj12od.mongodb.net/${databaseName}?retryWrites=true&w=majority`
 mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
@@ -21,20 +30,19 @@ const person = new Person({
 });
 
 
-if (process.argv.length < 3) {
-  console.log("Current argv length", process.argv.length);
-  console.log(
-    `Please provide the password as an argument: node mongo.js <password>`
-  );
-  process.exit(1);
-} else if (process.argv.length === 3) {
-  Person.find({ important: true }).then((result) => {
+if (process.argv.length === 3) {
+  console.log("Starting find function...");
+  console.log("Current processes...", process.argv);
+  Person.find({}).then((result) => {
+    console.log("Result: ");
     result.forEach((person) => {
       console.log(person);
     });
     mongoose.connection.close();
   });
-} else if (process.argv.length > 3 && process.argv.length > 5) {
+} else if (process.argv.length > 3 && process.argv.length <= 5) {
+  console.log("Commencing new entry...");
+  console.log("Current processes...", process.argv);
   person.save().then((result) => {
     console.log(`Added ${name} number ${number} to phonebook`);
     mongoose.connection.close();
