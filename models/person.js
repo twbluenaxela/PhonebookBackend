11dafd4af64b54269107b692f401ef39ba5e4c83
Nick Yu <rednaxela64@outlook.com>
2022-08-01 17:32:17 -0700

    Fix regex for ph num validator

--- models/person.js
+++ models/person.js
@@ -24,7 +24,7 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function(v) {
-        return /^\d{2,3}-\d{5}$/.test(v)
+        return /^\d{2,3}-\d{5}/.test(v)
       }
     },
     required: true
